5565a6e2cb89f32bf0598b70ef9202aa707c2658
Ilya <ilya.velikolpnyyy@mail.ru>
2021-11-22 02:54:58 +0300

    first commit

---
+++ Assets/Scripts/MoveJob.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Burst;
+
+[BurstCompile]
+public struct MoveJob : IJobParallelForTransform
+{
+    public NativeArray<Vector3> Positions;
+    public NativeArray<Vector3> Velocities;
+    public NativeArray<Vector3> Accelerations;
+    public float DeltaTime;
+    public float VelocityLimit;
+
+    public void Execute(int index, TransformAccess transform)
+    {
+        var velocity = Velocities[index] + Accelerations[index] * DeltaTime;
+        var direction = velocity.normalized;
+        velocity = direction * Mathf.Clamp(velocity.magnitude, 1, VelocityLimit);
+        transform.position += velocity * DeltaTime;
+        transform.rotation = Quaternion.LookRotation(direction);
+
+        Positions[index] = transform.position;
+        Velocities[index] = velocity;
+        Accelerations[index] = Vector3.zero;
+    }
+}
